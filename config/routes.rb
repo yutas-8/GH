fa1a8d75fb15b4a34225d4dc1d0a9ea850bcf2a9
EC2 Default User <gh01.yuta@gmail.com>
2020-11-08 02:40:14 +0000

    [add]devise,header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :members
+  devise_for :members, controllers: {
+    sessions: "user/members/sessions",
+    registrations: "user/members/registrations"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
