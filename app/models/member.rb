3da589978d53a4c0bf2b577d2e8835b37e5412c8
EC2 Default User <gh01.yuta@gmail.com>
2020-11-17 13:55:29 +0000

    [add]管理者ログイン、社員情報一覧、論理削除＆アカウント削除

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,19 +4,24 @@ class Member < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
-  has_many :posts#dependent: :destroyを外しておく。memberが消えてもpostは残るようにしたいと考えている為
-  has_many :post_comments
+  has_many :posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   #ありがとうを受け取る側
-  has_many :to_thanks, class_name: "Thank", foreign_key: "to_id"
+  has_many :to_thanks, class_name: "Thank", foreign_key: "to_id", dependent: :destroy
   has_many :tos, through: :to_thanks, source: :from
   #ありがとうを贈る側
-  has_many :from_thanks, class_name: "Thank", foreign_key: "from_id"
+  has_many :from_thanks, class_name: "Thank", foreign_key: "from_id", dependent: :destroy
   has_many :froms, through: :from_thanks, source: :to
-  
-  has_many :praises
-  has_many :cheerings
-  
-  
+
+  has_many :praises, dependent: :destroy
+  has_many :cheerings, dependent: :destroy
+
+  #退職memberはログイン出来ない
+  def active_for_authentication?
+    super && (self.is_delete == false)
+  end
+
+
 
 end
