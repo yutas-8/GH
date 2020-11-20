9da626c8e97aa9f06babb337f99d0bc30dc17e5b
EC2 Default User <gh01.yuta@gmail.com>
2020-11-20 07:56:11 +0000

    [update]thank:index,tos,froms home:top

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -5,12 +5,12 @@ class User::HomeController < ApplicationController
     @this_month_thanks = Thank.where("cast(strftime('%m', created_at) as int) = ?", Time.now.month)
     @prev_month_thanks = Thank.where("cast(strftime('%m', created_at) as int) = ?", Time.now.prev_month.month)
     @month_to_month_thanks = @this_month_thanks.count -  @prev_month_thanks.count
-    @thanks = Thank.where(to_id: current_member.id).order(created_at: :desc).limit(5)
+    @thanks = Thank.where(to_id: current_member.id).order(created_at: :desc).limit(3)
     # 誕生日
     @birthday_members = Member.where("cast(strftime('%m', birthday) as int) = ?", Time.now.month)
     @next_birthday_members = Member.where("cast(strftime('%m', birthday) as int) = ?", Time.now.next_month.month)
     # 投稿
-    @posts = Post.order(created_at: :desc).limit(5)
+    @posts = Post.order(created_at: :desc).limit(3)
 
   end
 end
