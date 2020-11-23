a9c8d0bee86c0c7bdf8ca7f476ab90a439982a6e
EC2 Default User <gh01.yuta@gmail.com>
2020-11-23 14:32:24 +0000

    [Fix]画像のシークレットkeyを追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = "551ac84b62d9ab3264d1681ab4673cfc454e3225a813be4f2cd556371c1d5779f4b1121b50b57dea9e70c5e122c8bdd2d15063793f1d2b498a5e6251ccfacc28"
