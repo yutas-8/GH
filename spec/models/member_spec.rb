96f23ca9d1063d682d0a84f41439295164dddfb2
EC2 Default User <gh01.yuta@gmail.com>
2020-12-23 03:56:20 +0000

    テスト記述練習

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  context "ユーザー登録" do
+    before do
+      @member = Member.new
+      @member.first_name = "1"
+      @member.last_name = "1"
+      @member.email = "1@1"
+      @member.encrypted_password = "111111"
+      @member.save
+    end
+    it "作成された" do
+      expect(@member).to be_valid
+    end
+  end
+  
+  context "バリデーションテスト" do
+    before do
+      @member = Member.new
+      @member.first_name = ""
+      @member.last_name = ""
+      @member.email = ""
+      @member.encrypted_password = ""
+      @member.save
+    end
+    it "動作している" do
+      expect(@member).not_to be_valid
+    end
+  end
 end
